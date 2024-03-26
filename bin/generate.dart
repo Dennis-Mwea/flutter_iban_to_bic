6d8fff82d0cd0543cc71cd4513716662c8a451f8
Dennis Mwea <mweadennis2@gmail.com>
2024-03-26 03:38:07 +0300

    Revert test

--- bin/generate.dart
+++ bin/generate.dart
@@ -1,14 +1,10 @@
-import 'dart:io';
-
 import 'package:iban_to_bic/src/generators/at.dart';
+import 'package:iban_to_bic/src/generators/be.dart';
+import 'package:iban_to_bic/src/generators/de.dart';
+import 'package:iban_to_bic/src/generators/fr_es.dart';
+import 'package:iban_to_bic/src/generators/lu.dart';
+import 'package:iban_to_bic/src/generators/nl.dart';
 
 void main() async {
-  print('Dir: ${Directory.current}');
-  final Uri script = Platform.script;
-  final Directory scriptDir = Directory(script.toFilePath());
-  print('Dir: ${scriptDir.parent}');
-  await Future.wait(<Future<void>>[
-    at(),
-    // be(), de(), frEs(), lu(), nl()
-  ]);
+  await Future.wait(<Future<void>>[at(), be(), de(), frEs(), lu(), nl()]);
 }
